794b41dbf7eb515fdae0c501afcfbd3af80d83e5
Nathan Krasney <natankrasney@gmail.com>
2020-06-30 09:16:00 +0300

    first version

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReactHookFormSample from './ReactHookFormSample'
+
+function App() {
+  return (
+    <div className="App">
+      <ReactHookFormSample/>
+    </div>
+  );
+}
+
+export default App;
