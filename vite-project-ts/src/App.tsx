92a2661e9e1b76cdee371794fbbe9594932ba204
Nathan Krasney <natankrasney@gmail.com>
2023-01-27 12:10:20 +0200

    form with controller for mui

--- vite-project-ts/src/App.tsx
+++ vite-project-ts/src/App.tsx
@@ -1,10 +1,14 @@
 import ContactUsVer1 from "./components/contact-us-ver1";
+import { FormWithControllerForMui } from "./components/FormWithControllerForMui";
 import ReactHookFormSample3 from "./components/ReactHookFormSample3";
 import ReactHookFormSample4 from "./components/ReactHookFormSample4";
 
 function App() {
   return (
     <div className="App">
+      <h2>FormWithMui</h2>
+      <span>check the console on submit</span>
+      <FormWithControllerForMui />
       <h2>ContactUs</h2>
       <span>check the console on submit</span>
       <ContactUsVer1 />
---
+++ vite-project-ts/src/components/FormWithControllerForMui.tsx
@@ -0,0 +1,28 @@
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { Checkbox } from "@mui/material";
+
+interface IFormInputs {
+  TextField: string;
+  MyCheckbox: boolean;
+}
+
+export function FormWithControllerForMui() {
+  const { handleSubmit, control } = useForm<IFormInputs>({
+    defaultValues: {
+      MyCheckbox: false,
+    },
+  });
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="MyCheckbox"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => <Checkbox {...field} />}
+      />
+      <input type="submit" />
+    </form>
+  );
+}
